8e84cb5a585a5b8a62017dc3f7826727fd37892b
lec <seawhirp@gmail.com>
2013-02-22 11:08:37 +0800

    шинээр image_gallery файл үүсгэлээ

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageGallery::Application.config.secret_token = '1644212458d589b3bdd832a666ebf5345d40b03dec90d8b6865b1467fcb2e23e9d0dbf1b7f9e3df963f6fa102ed3a53cdfcfa30e8abf2e5c32e0054d324f1349'
